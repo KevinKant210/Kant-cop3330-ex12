ccab70687804d83aa048f7a8f068b604d84089f8
Kevin Desktop <kevinkant@knights.ucf.edu>
2021-06-03 18:37:44 -0400

    wrhuiowherfuhifhueif

--- untitled/E_12/src/main/java/org/example/CalculateInterest.java
+++ untitled/E_12/src/main/java/org/example/CalculateInterest.java
@@ -7,14 +7,15 @@ import java.text.DecimalFormat;
 public class CalculateInterest {
     Scanner reader = new Scanner(System.in);
     DecimalFormat formatter = new DecimalFormat("###.##");
-    float principle,interest;
+    float principle,interest,interestholder;
     float numyears;
 
     public void getValues(){
         System.out.print("Enter the the principle: ");
         principle = reader.nextFloat();
         System.out.print("Enter the Interest rate as a whole number:" );
-        interest = reader.nextFloat()/100;
+        interestholder = reader.nextFloat();
+        interest = interestholder/100;
         System.out.print("Enter the number of years: ");
         numyears = reader.nextFloat();
     }
@@ -22,7 +23,7 @@ public class CalculateInterest {
     public void calcInterest() {
         float total = principle * (1 + (numyears * interest));
         total = Float.parseFloat(formatter.format(total));
-        System.out.printf("After %.0f years at  %.2f%% interest, the investment will be worth %.2f", numyears,interest,total);
+        System.out.printf("After %.0f years at  %.1f%% interest, the investment will be worth $%.2f", numyears,interestholder,total);
     }
 
 }
